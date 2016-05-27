9628b7469eaa3f266cb37b48502bb00955eeb714
samer-- <s.abdallah@ucl.ac.uk>
2016-05-27 16:42:52 +0100

    Remove dependence on dcgutils

--- pack.pl
+++ pack.pl
@@ -3,5 +3,4 @@ version('0.7.0').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Skippable pseudorandom generator and distributions').
 download('https://github.com/samer--/plrand.git').
-requires(dcgutils).
 
