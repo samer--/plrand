6f2eb4c18fc206d23b07dd94f8c737435e10bf28
samer-- <s.abdallah@ucl.ac.uk>
2017-03-30 15:41:51 +0100

    Bump

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(plrand).
-version('0.9.8').
+version('0.9.9').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Skippable pseudorandom generator and distributions').
 download('https://github.com/samer--/plrand.git').
